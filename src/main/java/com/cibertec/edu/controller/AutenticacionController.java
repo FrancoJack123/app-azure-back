4d6427f3777090f6f8dd1680b4c90d1b0bae9036
FrancoJack123 <jackfrancomoreno15@gmail.com>
2024-10-26 13:48:02 -0500

    3

--- src/main/java/com/cibertec/edu/controller/AutenticacionController.java
+++ src/main/java/com/cibertec/edu/controller/AutenticacionController.java
@@ -21,7 +21,7 @@ public class AutenticacionController {
         try {
             UsuarioDTO usuario = autenticacionService.validarUsuario(loginRequestDTO);
             if (usuario != null) {
-                return new LoginResponseDTO(usuario, "Login exitoso1");
+                return new LoginResponseDTO(usuario, "Login exitoso2");
             } else {
                 return new LoginResponseDTO(null, "Problemas en la autenticación");
             }
